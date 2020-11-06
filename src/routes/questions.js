eb347dbb3a9708cdac5f7c803b390e8ae50d95f1
SoHyun <pje1740@gmail.com>
2020-11-06 17:32:10 +0900

    [FIX] fix the logic inside retreiving the questions data
    Changed reduce function to map and use Promise.all to resolve all the promises.
    Reduce returns a single value while map returns a new array.

--- src/routes/questions.js
+++ src/routes/questions.js
@@ -1,22 +1,18 @@
-/* eslint-disable no-param-reassign */
 const express = require('express');
 
 const router = express.Router();
 const pool = require('../models/DbConnection');
 
 router.get('', async (req, res) => {
-  const questions = await pool.query('SELECT qid, content FROM questions');
-  const questionsIter = questions[0];
-  const result = await [...questionsIter].reduce(async (lastPromise, q) => {
-    await lastPromise;
-    const temp = await pool.query(
+  const [questions] = await pool.query('SELECT qid, content FROM questions');
+  const result = questions.map(async (q) => {
+    const [choices] = await pool.query(
       `SELECT content, type1, type2, type3, type4, type5 FROM choices WHERE qid=${q.qid}`,
     );
-    // eslint-disable-next-line prefer-destructuring
-    q.choices = temp[0];
-    return q;
-  }, Promise.resolve());
-  res.status(200).json({ result });
+    return { ...q, choices };
+  });
+  const resolved = await Promise.all(result);
+  res.status(200).json({ resolved });
 });
 
 module.exports = router;
