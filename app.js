6c18fa0f633e5c3408a049844cf861c35b5f116f
SoHyun <pje1740@gmail.com>
2020-11-07 13:44:07 +0900

    [FIX] add cors
    Cors module is required in order to fix CORS error in the client side.
    I added cors module and added lines in app.js

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 const createError = require('http-errors');
 const express = require('express');
-const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const cors = require('cors');
 
 const questionsRouter = require('./src/routes/questions');
 const usersRouter = require('./src/routes/users');
@@ -14,6 +14,8 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+app.use(cors());
 
 app.use('/questions', questionsRouter);
 app.use('/users', usersRouter);
